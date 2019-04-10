8bad339393a95c6197cb1861b045798e877552e1
Filipe Medeiros <fcma@cin.ufpe.br>
2019-04-09 21:37:45 -0300

    Improving PhytoProm dataviz

--- PhytoProm/js/main.js
+++ PhytoProm/js/main.js
@@ -1,65 +1,82 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    3.2 - Linear scales
+/**
+ * main.js
+ * Filipe Cordeiro de Medeiros Azevedo
+ * Cis-Element visualization by promoter
 */
 
-var svg = d3.select("#chart-area")
-    .append("svg")
-        .attr("width", "400")
-        .attr("height", "400");
+var margin = {'left': 150, 'right': 10, 'top': 10, 'bottom': 100};
 
-d3.json("data/test.json").then(function(data){
+var width = 900 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
 
-    data.forEach(d => {
-        d.upstream = +d.upstream;
-    });
+var g = d3.select("body")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + margin.left 
+                + ", " + margin.top + ")")
+
+d3.json("data/dataset.json").then(function(data){
     console.log(data);
 
     var y = d3.scaleBand()
         .domain(data.map(function(d){
             return d.promoter;
         }))
-        .range([0, 400])
+        .range([0, height])
         .paddingInner(0.2)
         .paddingOuter(0.2);
 
     var x = d3.scaleLinear()
         .domain([0,1000])
-        .range([0,400]);
+        .range([0,width]);
 
-    var line = svg.selectAll("line")
+    var line = g.selectAll("line")
         .data(data);
-    
+
     line.enter()
         .append('line')
             .attr('x1', 0)
             .attr('y1', (d,i)=>y(d.promoter))
-            .attr('x2', 400)
+            .attr('x2', width)
             .attr('y2', (d,i)=>y(d.promoter))
             .attr('stroke-width', 1)
             .attr('stroke', 'black');
 
 
-    var circle = svg.selectAll("circle")
-            .data(data)
+    var circleGroups = g.selectAll("g.circles")
+    .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "circles");
+
+    var circle = circleGroups.selectAll("circle")
+            .data(getCircleData)
         .enter()
             .append("circle")
             .attr("cy", function(d){
                 return y(d.promoter)
             })
             .attr("cx", function(d){
-               return x(d.upstream)
+            return x(d.upstream)
             })
             .attr("r", 5)
             .attr("fill", function(d) {
                 return "black";
             });
-    
 
-}).catch(function(error){
-    console.log(error)
+    function getCircleData(d) {
+    var cdata = d.upstream.map (function(ele) { 
+        return {upstream: ele, promoter: d.promoter};
     });
-
-
-
+    return cdata; 
+    }
+    
+    function getCircleData(d) {
+        var cdata = d.upstream.map (function(ele) { 
+            return {upstream: ele, promoter: d.promoter};
+        });
+        return cdata; 
+        }
+});
